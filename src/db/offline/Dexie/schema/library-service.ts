d9cb81797a1771cfc84204057987d32a64cdc6ae
Axl Rose G. Villanueva <62780935+axlrose016@users.noreply.github.com>
2025-06-09 16:08:55 +0800

    staging deployment 06-09-2025
    v5

--- src/db/offline/Dexie/schema/library-service.ts
+++ src/db/offline/Dexie/schema/library-service.ts
@@ -96,34 +96,34 @@ export const seedRoles: IRoles[] = [
         "is_deleted": false,
         "remarks": "Seeded"
     },
-    {
-        "id": "17eb1f81-83d3-4642-843d-24ba3e40f45c",
-        "role_description": "Finance",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    },
-    {
-        "id": "1c99504f-ad53-4151-9a88-52e0cffdbb6d",
-        "role_description": "Engineer",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    },
+    // {
+    //     "id": "17eb1f81-83d3-4642-843d-24ba3e40f45c",
+    //     "role_description": "Finance",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
+    // {
+    //     "id": "1c99504f-ad53-4151-9a88-52e0cffdbb6d",
+    //     "role_description": "Engineer",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
     {
         "id": "37544f59-f3ba-45df-ae0b-c8fa4e4ce446",
         "role_description": "CFW Beneficiary",
@@ -152,36 +152,35 @@ export const seedRoles: IRoles[] = [
         "is_deleted": false,
         "remarks": "Seeded"
     },
-    {
-        "id": "eed84e85-cd50-49eb-ab19-a9d9a2f3e374",
-        "role_description": "CFW Alternate Supervisor",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-
-    },
-    {
-        "id": "e2ebba79-7134-4ddb-838f-9350a89c2a0e",
-        "role_description": "CFW HEI Focal Person",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    }
-    ,
+    // {
+    //     "id": "eed84e85-cd50-49eb-ab19-a9d9a2f3e374",
+    //     "role_description": "CFW Alternate Supervisor",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
+    // {
+    //     "id": "e2ebba79-7134-4ddb-838f-9350a89c2a0e",
+    //     "role_description": "CFW HEI Focal Person",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // }
+    // ,
     {
         "id": "cf05023f-b2dc-46be-ab08-d82dfc8d8cd5",
         "role_description": "CFW Administrator",
@@ -222,21 +221,21 @@ export async function getModules() {
     }
 }
 export const seedModules: IModules[] = [
-    {
-        "id": "9bb8ab82-1439-431d-b1c4-20630259157a",
-        "module_description": "Sub-Project",
-        "module_path": "subproject",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    },
+    // {
+    //     "id": "9bb8ab82-1439-431d-b1c4-20630259157a",
+    //     "module_description": "Sub-Project",
+    //     "module_path": "subproject",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
     {
         "id": "4e658b02-705a-43eb-a051-681d54e22e2a",
         "module_description": "Person Profile",
@@ -252,51 +251,51 @@ export const seedModules: IModules[] = [
         "is_deleted": false,
         "remarks": "Seeded"
     },
-    {
-        "id": "19a18164-3a26-4ec3-ac6d-755df1d3b980",
-        "module_description": "Human Resource and Development",
-        "module_path": "hr-development",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    },
-    {
-        "id": "78ac69e0-19b6-40d0-8b07-135df9152bd8",
-        "module_description": "Procurement",
-        "module_path": "procurement",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    },
-    {
-        "id": "ce67be45-b5aa-4272-bcf4-a32abc9d7068",
-        "module_description": "Engineering",
-        "module_path": "engineering",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    },
+    // {
+    //     "id": "19a18164-3a26-4ec3-ac6d-755df1d3b980",
+    //     "module_description": "Human Resource and Development",
+    //     "module_path": "hr-development",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
+    // {
+    //     "id": "78ac69e0-19b6-40d0-8b07-135df9152bd8",
+    //     "module_description": "Procurement",
+    //     "module_path": "procurement",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
+    // {
+    //     "id": "ce67be45-b5aa-4272-bcf4-a32abc9d7068",
+    //     "module_description": "Engineering",
+    //     "module_path": "engineering",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // },
     {
         "id": "866e6bcb-041f-4d58-94bf-6c54e4855f85",
         "module_description": "Settings",
@@ -312,21 +311,21 @@ export const seedModules: IModules[] = [
         "is_deleted": false,
         "remarks": "Seeded"
     },
-    {
-        "id": "f8446068-385e-461c-9417-5caf086f103e",
-        "module_description": "Finance",
-        "module_path": "finance",
-        "created_by": "00000000-0000-0000-0000-000000000000",
-        "created_date": new Date().toISOString(),
-        "last_modified_by": "",
-        "last_modified_date": "",
-        "push_status_id": 2,
-        "push_date": "",
-        "deleted_by": "",
-        "deleted_date": "",
-        "is_deleted": false,
-        "remarks": "Seeded"
-    }
+    // {
+    //     "id": "f8446068-385e-461c-9417-5caf086f103e",
+    //     "module_description": "Finance",
+    //     "module_path": "finance",
+    //     "created_by": "00000000-0000-0000-0000-000000000000",
+    //     "created_date": new Date().toISOString(),
+    //     "last_modified_by": "",
+    //     "last_modified_date": "",
+    //     "push_status_id": 2,
+    //     "push_date": "",
+    //     "deleted_by": "",
+    //     "deleted_date": "",
+    //     "is_deleted": false,
+    //     "remarks": "Seeded"
+    // }
 ]
 
 //Permissions Service
